e554cfdb2970384e35231dcb6ec0ede2cb9c472c
ahmad941 <ahmadmubarok941@gmail.com>
2024-05-04 08:43:56 +0700

    first

---
+++ Src/index.php
@@ -0,0 +1,16 @@
+<?php
+
+// require "Mobil/Mobil.php";
+// require "Mobil/Toyota.php";
+// require "Mobil/Honda.php";
+// require "Calculator.php";
+
+require "vendor/autoload.php";
+
+$Toyota = new Dummy\Mobil\Toyota(" Toyota Fortuner ", 10);
+$calculator = new Dummy\Calculator($Toyota);
+echo "jarak maksimum ".$Toyota->getMerk().$calculator->hitungJarak()."Km";
+echo "<hr>";
+$Honda = new Dummy\Mobil\Honda("Honda CRV", 10);
+$calculator = new Dummy\Calculator($Honda);
+echo "jarak maksimum ".$Honda->getMerk().$calculator->hitungJarak()."Km";
